dc65283dc07c821dd42a2a8acb4ea1edd31d7259
Kim Miso <kimms9725@naver.com>
2022-05-11 14:50:29 +0900

    mongoDB

--- 0509/express/myapp/routes/post.js
+++ 0509/express/myapp/routes/post.js
@@ -1,23 +1,61 @@
 const express = require("express");
+const BookSchema = require("../models/book");
 const router = express.Router();
 
 router.get("/", (req, res) => {
   res.render("post");
 });
 
-router.post("/", (req, res, next) => {
-  const name = req.body.name;
-  const phone = req.body.phone;
-  const date = req.body.date;
+router.get("/bookinfo/:id", (req, res) => {
+  const authorname = req.params.id;
 
-  // 웹 통신 방식 1요청 1응답 -> 통신 종료되는게 정상
-  // res.json({ name: name, phone: phone, date: date });
-
-  next();
+  // BookSchema.find({ author: authorname }, (err, result) => {
+  //   if (result) {
+  //     return res.json(result);
+  //   } else {
+  //     return res.send("등록된 작가가 없습니다");
+  //   }
+  // });
+  BookSchema.find({ author: authorname })
+    .then(result => {
+      res.json(result);
+    })
+    .catch(err => {
+      console.log(err);
+    });
 });
 
-router.post("/", (req, res) => {
+// router.post("/", (req, res, next) => {
+//   const name = req.body.name;
+//   const phone = req.body.phone;
+//   const date = req.body.date;
+
+//   // 웹 통신 방식 1요청 1응답 -> 통신 종료되는게 정상
+//   res.json({ name: name, phone: phone, date: date });
+
+//   // next();
+// });
+
+//  '/' ==> expost/addbook
+router.post("/addbook", (req, res) => {
+  const bookname = req.body.bookname;
+  const author = req.body.author;
+  const price = req.body.price;
+  const publish = req.body.publish;
+
+  let bookData = new BookSchema({
+    bookname: bookname,
+    author: author,
+    price: price,
+    publish: publish,
+  });
+
+  bookData.save();
   res.redirect("/expost");
 });
+
+// router.post("/", (req, res) => {
+//   res.redirect("/expost");
+// });
 
 module.exports = router;
