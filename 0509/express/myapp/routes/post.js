a0359da2eefeaa26428b1ef564f733c785809355
Kim Miso <kimms9725@naver.com>
2022-05-11 11:29:55 +0900

    1요청 1응답

--- 0509/express/myapp/routes/post.js
+++ 0509/express/myapp/routes/post.js
@@ -5,11 +5,19 @@ router.get("/", (req, res) => {
   res.render("post");
 });
 
-router.post("/", (req, res) => {
+router.post("/", (req, res, next) => {
   const name = req.body.name;
   const phone = req.body.phone;
   const date = req.body.date;
-  res.json({ name: name, phone: phone, date: date });
+
+  // 웹 통신 방식 1요청 1응답 -> 통신 종료되는게 정상
+  // res.json({ name: name, phone: phone, date: date });
+
+  next();
+});
+
+router.post("/", (req, res) => {
+  res.redirect("/expost");
 });
 
 module.exports = router;
