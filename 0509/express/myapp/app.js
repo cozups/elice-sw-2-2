f5be7a4e2b577e41a323eade49c291217baca6d4
Kim Miso <kimms9725@naver.com>
2022-05-16 13:34:00 +0900

    cors 학습

--- 0509/express/myapp/app.js
+++ 0509/express/myapp/app.js
@@ -3,6 +3,7 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+const cors = require("cors");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
@@ -16,6 +17,13 @@ var app = express();
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH"],
+  })
+);
 
 app.use(logger("dev"));
 app.use(express.json());
